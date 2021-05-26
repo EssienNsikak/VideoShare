8be11c19b26eabc66fe936230038da2d7e6fed1c
the-ceo22 <nsikak.essienceo@gmail.com>
2021-05-26 15:16:58 +0100

    first commit

---
+++ src/components/categoriesBar/CategoriesBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './_categoriesBar.scss';
+
+const CategoriesBar = () => {
+  return (
+    <div>
+      <h6>Categories Bar</h6>
+    </div>
+  )
+}
+
+export default CategoriesBar
---
+++ src/components/video/Video.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './_video.scss';
+
+const Video = () => {
+  return (
+    <div>
+      <p>Videos Component</p>
+    </div>
+  )
+}
+
+export default Video
