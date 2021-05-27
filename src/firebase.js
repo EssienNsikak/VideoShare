3bdeb1465ce9e0c3a4c15873b1eeba0c03aa4819
the-ceo22 <nsikak.essienceo@gmail.com>
2021-05-27 14:06:26 +0100

    firebase added

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBMtmI42Hsm2f3Mc0-IXMXQe2Hm_nhVchg",
+  authDomain: "videoshare-app.firebaseapp.com",
+  projectId: "videoshare-app",
+  storageBucket: "videoshare-app.appspot.com",
+  messagingSenderId: "668869318980",
+  appId: "1:668869318980:web:1f729ccc62b15708b29f11"
+};
+
+firebase.initializeApp(firebaseConfig);
