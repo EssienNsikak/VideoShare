ec7984c7fff9056abd7eabf627ca6d968161db64
the-ceo22 <nsikak.essienceo@gmail.com>
2021-05-27 13:35:12 +0100

    loginscreen added and react-router added

--- src/App.js
+++ src/App.js
@@ -4,28 +4,64 @@ import Header from './components/header/Header';
 import Sidebar from './components/sidebar/Sidebar';
 import HomeScreen from './screen/homeScreen/HomeScreen';
 import './_app.scss';
+import LoginScreen from './screen/loginScreen/LoginScreen';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
-const App = () => {
+
+const Layout = ({ children }) => {
 
   const [sidebar, toggleSidebar] = useState(false)
 
-   const handleToggleSidebar = () => toggleSidebar(value => !value)
+  const handleToggleSidebar = () => toggleSidebar(value => !value)
+  
+  return (
+    <>
+      <Header handleToggleSidebar={handleToggleSidebar} />
+      <div className='app__container'>
+    
+        <Sidebar 
+          sidebar={sidebar}
+          handleToggleSidebar={handleToggleSidebar} 
+        />
+        <Container fluid className='app_main'>
+          {children}
+        </Container>
+        
+      </div>    
+    </>
+  )
+}
 
-  return <>
-    <Header handleToggleSidebar={handleToggleSidebar} />
-    <div className='app__container'>
-  
-      <Sidebar 
-        sidebar={sidebar}
-        handleToggleSidebar={handleToggleSidebar} 
-      />
-      <Container fluid className='app_main'>
-        <HomeScreen />
-      </Container>
-      
-    </div>    
-  </>
-  
+const App = () => {
+
+  return <Router>
+
+    <Switch>
+
+      <Route path='/' exact>
+        <Layout>
+          <HomeScreen />
+        </Layout>
+      </Route>
+
+      <Route path='/login'>
+        <LoginScreen />
+      </Route>
+
+      <Route path='/search'>
+        <Layout>
+          <h1>Search Results</h1>
+        </Layout>
+      </Route>
+
+      <Route>
+        <Redirect to='/' />
+      </Route>
+
+    </Switch>  
+
+  </Router>
+
 }
 
 export default App
--- src/screen/loginScreen/LoginScreen.js
+++ src/screen/loginScreen/LoginScreen.js
@@ -5,13 +5,14 @@ const LoginScreen = () => {
   return (
     <div className='login'>
       <div className='login__container'>
+        <h2>VideoShare</h2>      
         <img 
           className='' 
           src='http://pngimg.com/uploads/youtube/youtube_PNG2.png' 
           alt='' 
         />
         <button>Login With Google</button>
-        <p>VideoShare - Create And Share Videos With Your Friends Around The World!</p>
+        <p>Create And Share Videos With Your Friends Around The World!</p>
       </div>
     </div>
   )
